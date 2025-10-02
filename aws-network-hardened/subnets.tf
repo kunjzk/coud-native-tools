80f18ea65c93eb9faba1099f1ea1478e58fe1157
Kunal Katarya <kunal.rox@gmail.com>
2025-10-02 19:48:47 +0800

    curl to nginx works

--- aws-network-hardened/subnets.tf
+++ aws-network-hardened/subnets.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_second" {
 resource "aws_subnet" "private" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
-  availability_zone = "ap-southeast-1c"
+  availability_zone = "ap-southeast-1a"
   tags = merge(local.tags, {
     Name = "Private Subnet"
   })
